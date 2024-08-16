74a803ac9d0eb883cd1960760f52969700cea7ed
raphaelsiqueiira <raphaelborba10@gmail.com>
2024-08-16 19:12:42 -0300

    Desafio Atualizado

--- desafios/desafio_calculadora_partida_hankeada/index.js
+++ desafios/desafio_calculadora_partida_hankeada/index.js
@@ -4,21 +4,21 @@ function saldoHankeadas(quantVitorias, quantDerrotas) {
 
   if (saldoVitoriaDerrota < 10) {
     classificacao = "Ferro";
-  } else if (saldoHankeadas < 20) {
+  } else if (saldoVitoriaDerrota < 20) {
     classificacao = "Bronze";
-  } else if (saldoHankeadas < 50) {
+  } else if (saldoVitoriaDerrota < 50) {
     classificacao = "Prata";
-  } else if (saldoHankeadas < 80) {
+  } else if (saldoVitoriaDerrota < 80) {
     classificacao = "Ouro";
   } else if (saldoHankeadas < 90) {
     classificacao = "Diamante";
-  } else if (saldoHankeadas < 10) {
+  } else if (saldoVitoriaDerrota < 100) {
     classificacao = "Lendário";
   } else {
     classificacao = "Imortal";
   }
 
-  console.log("Seu rank é " + classificacao);
+  console.log("Seu Herói tem de saldo de " + saldoVitoriaDerrota +  " está no Hank de " + classificacao);
 }
 
 saldoHankeadas(10, 8)
